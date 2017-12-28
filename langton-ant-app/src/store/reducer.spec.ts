8915d2cec1e9eeb479545e14f9b4502c7d99185a
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2017-12-28 14:16:02 +0100

    STEP 1 : add store

--- langton-ant-app/src/store/reducer.spec.ts
+++ langton-ant-app/src/store/reducer.spec.ts
@@ -2,7 +2,7 @@ import reducer from './reducer';
 import { Action } from 'redux';
 
 describe('reducer', () => {
-  it('should initialise with 1', () => {
+  it('should initialise with null', () => {
     const actual = reducer(undefined, { type: null} as Action);
     expect(actual).toBeNull();
   });
