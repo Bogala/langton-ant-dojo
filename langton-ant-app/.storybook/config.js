9d25194faea19095ca751835fcdfdfd6be57630c
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2017-12-28 16:26:19 +0100

    fix: update storybook configuration

--- langton-ant-app/.storybook/config.js
+++ langton-ant-app/.storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../src/stories');
+  const req = require.context('../src', true, /\.stories\.tsx$/)
+  req.keys().forEach((filename) => req(filename))
 }
 
 configure(loadStories, module);
+
