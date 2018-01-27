6bfc0e8ec54a28c46c2adc313ccbe85cade3f351
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2018-01-27 10:42:53 +0100

    STEP_4: reducer code implementation

--- langton-ant-app/src/store/reducer.spec.ts
+++ langton-ant-app/src/store/reducer.spec.ts
@@ -1,5 +1,29 @@
 import { MainState, Ant, default as  reducer } from './reducer';
 import { Action } from 'redux';
+import * as _ from 'lodash';
+
+interface GridCoordinates {
+  x: number;
+  y: number;
+}
+
+const initAndPlay = (playTimes: number = 1): MainState => {
+  const initialState = _.cloneDeep(reducer(undefined, { type: null} as Action));
+  let finalState = initialState;
+  for (let times = 0; times < playTimes; times++) {
+    finalState = reducer(finalState, { type: 'PLAY'} as Action);
+  }
+  return finalState;
+};
+
+const expectGreyCells = (context: MainState, ...greyCells: Array<GridCoordinates>) => {
+  for (let line = 0; line < context.grid.length; line++) {
+    for (let cell = 0; cell < context.grid[line].length; cell++) {
+      const val = greyCells.some(value => value.x === cell && value.y === line);
+      expect(context.grid[line][cell]).toBe(val);
+    }
+  }
+};
 
 describe('reducer', () => {
   test('should initialise with MainState Interface', () => {
@@ -10,6 +34,7 @@ describe('reducer', () => {
   test('should pass state by cdefault', () => {
     class MockMainState implements MainState {
       grid: boolean[][];
+      ant: Ant;
     }
     const actual = reducer(new MockMainState(), { type: null} as Action);
     expect(actual instanceof MockMainState).toBeTruthy();
@@ -22,7 +47,10 @@ describe('reducer', () => {
 
   test('initial state must have a 21x21xfalse grid', () => {
     const actual = reducer(undefined, { type: null} as Action);
-    expect(actual.grid).toEqual(new Array<Array<boolean>>(21).map(() => new Array<boolean>(21).fill(false)));
+    expect(actual.grid)
+      .toEqual(
+        new Array<Array<boolean>>(21).fill(new Array<boolean>(21)).map(() => new Array<boolean>(21).fill(false))
+      );
   });
 
   test('initial state must have an ant definition', () => {
@@ -33,5 +61,119 @@ describe('reducer', () => {
   test('initial state must have an ant at 10:10:0°', () => {
     const actual = reducer(undefined, { type: null} as Action);
     expect(actual.ant).toEqual(new Ant());
+  });
+
+  describe('[App]Step 3.1: First move', () => {
+
+    test('Ant must rotate 90° when play button clicked', () => {
+      const { ant } = initAndPlay();
+      expect(ant.rotation).toBe(90);
+    });
+
+    test('Cell is grey when play button clicked', () => {
+      expectGreyCells(initAndPlay(), { x: 10, y: 10 });
+    });
+
+    test('Ant must move left when play button clicked', () => {
+      const { ant } = initAndPlay();
+      expect(ant.x).toBe(11);
+      expect(ant.y).toBe(10);
+    });
+  });
+
+  describe('[App]Step 3.2: Second move', () => {
+    test('Ant must rotate 90° when play button clicked', () => {
+      const { ant } = initAndPlay(2);
+      expect(ant.rotation).toBe(180);
+    });
+
+    test('Cell is grey when play button clicked', () => {
+      expectGreyCells(initAndPlay(2), { x: 10, y: 10 }, { x: 11, y: 10 });
+    });
+
+    test('Ant must move left when play button clicked', () => {
+      const { ant } = initAndPlay(2);
+      expect(ant.x).toBe(11);
+      expect(ant.y).toBe(11);
+    });
+  });
+
+  describe('[App]Step 3.3: Third move', () => {
+    test('Ant must rotate 90° when play button clicked', () => {
+      const { ant } = initAndPlay(3);
+      expect(ant.rotation).toBe(270);
+    });
+
+    test('Cell is grey when play button clicked', () => {
+      expectGreyCells(initAndPlay(3), { x: 10, y: 10 }, { x: 11, y: 10 }, { x: 11, y: 11 });
+    });
+
+    test('Ant must move left when play button clicked', () => {
+      const { ant } = initAndPlay(3);
+      expect(ant.x).toBe(10);
+      expect(ant.y).toBe(11);
+    });
+  });
+
+  describe('[App]Step 3.4: Fourth move', () => {
+    test('Ant must rotate 90° when play button clicked', async () => {
+      const { ant } = await initAndPlay(4);
+      expect(ant.rotation).toBe(0);
+    });
+
+    test('Cell is grey when play button clicked', () => {
+      expectGreyCells(initAndPlay(4), { x: 10, y: 10 }, { x: 11, y: 10 }, { x: 11, y: 11 }, { x: 10, y: 11 });
+    });
+
+    test('Ant must move left when play button clicked', () => {
+      const { ant } = initAndPlay(4);
+      expect(ant.x).toBe(10);
+      expect(ant.y).toBe(10);
+    });
+  });
+
+  describe('[App]Step 3.5: Fifth move', () => {
+    test('Ant must rotate 90° when play button clicked', () => {
+      const { ant } = initAndPlay(5);
+      expect(ant.rotation).toBe(270);
+    });
+
+    test('Cell is grey when play button clicked', () => {
+      expectGreyCells(
+        initAndPlay(5),
+        { x: 11, y: 10 },
+        { x: 11, y: 11 },
+        { x: 10, y: 11 });
+    });
+
+    test('Ant must move left when play button clicked', () => {
+      const { ant } = initAndPlay(5);
+      expect(ant.x).toBe(9);
+      expect(ant.y).toBe(10);
+    });
+  });
+
+  describe('[App]Step 3.10: Tenth move', () => {
+    test('Ant must rotate 90° when play button clicked', () => {
+      const { ant } = initAndPlay(10);
+      expect(ant.rotation).toBe(180);
+    });
+
+    test('Cell is grey when play button clicked', () => {
+      expectGreyCells(
+        initAndPlay(10),
+        { x: 10, y: 10 },
+        { x: 11, y: 10 },
+        { x: 11, y: 11 },
+        { x: 10, y: 11 },
+        { x: 9, y: 9 },
+        { x: 10, y: 9 });
+    });
+
+    test('Ant must move left when play button clicked', () => {
+      const { ant } = initAndPlay(10);
+      expect(ant.x).toBe(9);
+      expect(ant.y).toBe(11);
+    });
   });
 });
--- langton-ant-app/src/store/reducer.ts
+++ langton-ant-app/src/store/reducer.ts
@@ -1,4 +1,5 @@
 import { Action } from 'redux';
+import * as _ from 'lodash';
 
 export class Ant {
   public x: number;
@@ -6,9 +7,9 @@ export class Ant {
   public rotation: number;
 
   constructor(x: number = 10, y: number = 10, rotation: number = 0) {
-      this.x = x;
-      this.y = y;
-      this.rotation = rotation;
+    this.x = x;
+    this.y = y;
+    this.rotation = rotation;
   }
 }
 
@@ -17,14 +18,69 @@ export interface MainState {
   ant: Ant;
 }
 
+const initGrid = () => (
+  new Array<Array<boolean>>(21).fill(new Array<boolean>(21))
+    .map(() => new Array<boolean>(21).fill(false))
+);
+
 const initialState: MainState = {
-  grid: new Array<Array<boolean>>(21).map(() => new Array<boolean>(21).fill(false)),
+  grid: initGrid(),
   ant: new Ant()
 };
 
 export default (state: MainState = initialState, action: Action) => {
   switch (action.type) {
+    case 'PLAY': {
+      const finalState = play(state);
+      return { ...finalState };
+    }
     default:
       return state;
   }
+};
+
+const play = ({ grid, ant }: MainState): MainState => {
+  const movement = moveByRotation(ant.rotation, grid[ant.y][ant.x]);
+  const rotation = newRotation(ant.rotation, grid[ant.y][ant.x]);
+  grid[ant.y][ant.x] = !grid[ant.y][ant.x];
+  movement.x += ant.x;
+  movement.y += ant.y;
+  return {
+    ant: { ...ant, rotation: rotation, x: movement.x, y: movement.y },
+    grid: [...grid]
+  };
+};
+
+const newRotation = (rotation: number, right: boolean) => {
+  let result = rotation + 90;
+  if (right) {
+    result += 180;
+  }
+  if (result >= 360) {
+    result -= 360;
+  }
+  return result;
+};
+
+const moveByRotation = (rotation: number, right: boolean) => {
+  const value = { x: 0, y: 0 };
+  switch (rotation) {
+    case 90:
+      value.y++;
+      break;
+    case 180:
+      value.x--;
+      break;
+    case 270:
+      value.y--;
+      break;
+    default:
+      value.x++;
+      break;
+  }
+  if (right) {
+    value.x = -value.x;
+    value.y = -value.y;
+  }
+  return value;
 };
