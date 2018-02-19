a22a3c860518f9ad775d81c964b2a275cdb6ca21
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2018-02-19 14:34:19 +0100

    STEP_5: update tests on epic

--- langton-ant-app/src/store/epic.spec.ts
+++ langton-ant-app/src/store/epic.spec.ts
@@ -1,16 +1,57 @@
-import { ActionsObservable } from 'redux-observable';
+import 'core-js';
+import 'jest-enzyme';
+
+import * as Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+import { createEpicMiddleware } from 'redux-observable';
+
+import configureStore, { MockStore } from 'redux-mock-store';
 import 'rxjs/Rx';
-import { PLAY, PLAYED } from './actions';
+import { PLAY, PLAYED, PAUSED } from './actions';
 import epic from './epic';
+import { Ant } from '.';
+
+configure({ adapter: new Adapter() });
+
+jest.useFakeTimers();
+
+const epicMiddleware = createEpicMiddleware(epic);
+const mockStore = configureStore([epicMiddleware]);
+
+let store: MockStore<{}>;
 
 describe('Epic', () => {
-    test('Dispatch played when launched', (done) => {
-        const action$ = ActionsObservable.of({ type: PLAY });
-        const expectedOutputActions = { type: PLAYED };
-
-        epic(action$).subscribe(actionReceived => {
-            expect(actionReceived.type).toBe(expectedOutputActions.type);
-            done();
+    beforeEach(() => {
+        store = mockStore({
+            grid: new Array<Array<boolean>>(21).fill(new Array<boolean>(21))
+                .map(() => new Array<boolean>(21).fill(false)),
+            ant: new Ant()
         });
+    });
+
+    afterEach(() => {
+        epicMiddleware.replaceEpic(epic);
+      });
+
+    test('Dispatch played when launched', () => {
+        store.dispatch({ type: PLAY });
+        jest.runOnlyPendingTimers();
+        expect(store.getActions()).toEqual([
+            { type: PLAY },
+            { type: PLAYED }
+          ]);
+    });
+
+    test('Dispatch cancelled when paused', () => {
+        store.dispatch({ type: PLAY });
+        jest.runOnlyPendingTimers();
+        store.dispatch({ type: PAUSED });
+        jest.runOnlyPendingTimers();
+        expect(store.getActions()).toEqual([
+            { type: PLAY },
+            { type: PLAYED },
+            { type: PLAYED },
+            { type: PAUSED }
+          ]);
     });
 });
