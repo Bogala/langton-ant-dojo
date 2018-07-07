89b7145e5c947d9803cebcb27625ac65967a1b54
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2018-07-07 23:22:09 +0200

    step1 before store

--- langton-app/src/App.tsx
+++ langton-app/src/App.tsx
@@ -1,22 +1,12 @@
+import { CircularProgress } from 'material-ui';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <MuiThemeProvider>
+    <div className="center">
+      <CircularProgress size={180} thickness={5} />
+    </div>
+  </MuiThemeProvider>
+);
