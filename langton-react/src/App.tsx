28aa3e7a3ef184d3d3529f515dcdc127f22bfc45
Benoit FONTAINE <benoit.a.fontaine@axa.fr>
2018-07-27 16:25:46 +0200

    refactor: add index on grid feature

--- langton-react/src/App.tsx
+++ langton-react/src/App.tsx
@@ -1,7 +1,7 @@
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import * as React from "react";
 import "./App.css";
-import Grid from "./grid/Grid";
+import { Grid } from "./grid";
 
 export default () => (
   <MuiThemeProvider>
